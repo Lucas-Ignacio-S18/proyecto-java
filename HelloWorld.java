e6ef1f14e735dad5479b781e95898d1a64bfa5a0
Lucas <lucasignaciosilva18@gmail.com>
2025-12-17 23:11:39 -0300

    primeros ejercicios en Java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hello World // Hola mundo");
     }
 }
+
+
+
+// git add → git commit → git push
---
+++ HelloWorldEx.java
@@ -0,0 +1,16 @@
+    public class HelloWorldEx {
+        public static void main(String[] args) {
+            //1.Imprime un mensaje que diga tu nombre en lugar de un "Hola mundo".
+            //Imprime mi nombre
+            System.out.println("Lucas");
+
+            //2.Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+            //Dos strings que sumandolos forman una oración
+            System.out.println("Hola" + " Mundo");
+
+            //3.Añade un comentario sobre lo que hace cada línea del programa".
+
+            //4.Crea un comentario en varias líneas.
+
+        }
+}
